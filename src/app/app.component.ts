370abbf4202139bc8128fd5c64f330faa2ee00a3
Damyan Peykov <dppeykov@gmail.com>
2019-12-11 13:30:19 +0100

    Event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,16 @@ import { Component } from "@angular/core";
 export class AppComponent {
   title: string = "Testing sandbox:";
   allowNewServer: boolean = false;
+  serverCreationStatus: string = "No server has been created!";
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+  }
+
+  // event binding function
+  onCreateServer() {
+    this.serverCreationStatus = "A new server has been created!";
   }
 }

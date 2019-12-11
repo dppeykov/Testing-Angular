1f25ac56a72d24a4c602bcd7a7bcf88715c5babe
Damyan Peykov <dppeykov@gmail.com>
2019-12-11 14:02:49 +0100

    Passing & using data with event binding
    Video 31 in section 2 - added the functionality of typing data in a form and using it in the app - this is before the two-way-databinding video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   title: string = "Testing sandbox:";
   allowNewServer: boolean = false;
   serverCreationStatus: string = "No server has been created!";
+  serverName: string = "";
 
   constructor() {
     setTimeout(() => {
@@ -20,4 +21,14 @@ export class AppComponent {
   onCreateServer() {
     this.serverCreationStatus = "A new server has been created!";
   }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
+  }
+
+  // can be done with explicit type casting - warning in eslint
+
+  // onUpdateServerName(event: Event) {
+  //   this.serverName = (<HTMLInputElement>event.target).value;
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+// we need the FormsModule to be able to use the 2-way binding
 
 import { AppComponent } from "./app.component";
 import { TestComponent } from "./test/test.component";
 
 @NgModule({
   declarations: [AppComponent, TestComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
